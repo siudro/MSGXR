02801de950a8330845186f82d4e96977f67c5ee8
Sedra Abdul Basir <83130573+siudro@users.noreply.github.com>
2024-07-22 20:04:29 +0300

    Update turki.py

--- turki.py
+++ turki.py
@@ -1,21 +1,25 @@
-
 import streamlit as st
 import openai
+
 st.title("اتكلم مع تركي")
-"""
+st.markdown("""
 انا الروبوت تركي وانا ساساعدكم بالتعرف على الثقافه السعوديه خصيصا منطقة الاحساء
-"""
+""")
+
 if "messages" not in st.session_state:
-        st.sessions_state["messages"] = [
-        {"role": "system", "content":"  اسمك تركي تحب استخدام الايموجيز عند الكلام وانت تنقل ثقافه المملكة العربيه السعوديه خصيصا منطقة الاحساء تكلم الجميع باحترام وترد على جميع من يقارن الاحساء بمنطقه اخرى "}
-        ]
-user = st.chat_input()
-if user:
+    st.session_state["messages"] = [
+        {"role": "system", "content": "اسمك تركي تحب استخدام الايموجيز عند الكلام وانت تنقل ثقافه المملكة العربيه السعوديه خصيصا منطقة الاحساء تكلم الجميع باحترام وترد على جميع من يقارن الاحساء بمنطقه اخرى"}
+    ]
+
+user_input = st.chat_input()
+
+if user_input:
     openai.api_key = st.secrets["api"]
-    st.session_state.message.append({"role": "user", "content": user})
-    st.chat_message("user").write(user)
-    response = openai.ChatCompletion.creat(model="gpt-4o-mini", messages=st.session_state.messages)
-    ai = response.choices[0].message
-    st.session_state.messages.append(ai)
-    st.chat_messag("assistant").write(ai.content)
+    st.session_state["messages"].append({"role": "user", "content": user_input})
+    st.chat_message("user").write(user_input)
     
+    response = openai.ChatCompletion.create(model="gpt-4", messages=st.session_state["messages"])
+    ai_response = response.choices[0].message
+    
+    st.session_state["messages"].append({"role": "assistant", "content": ai_response["content"]})
+    st.chat_message("assistant").write(ai_response["content"])
